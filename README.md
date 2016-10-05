211147a02eaf42a7288ab5e2b042ff2380d45a7a
Hayden <hayden@hkparker.com>
2016-10-04 23:10:03 -0700

    add realistic benchmarks

--- README.md
+++ README.md
@@ -4,13 +4,15 @@ Benchmarking embedded scripting languages inside Go, currently [Otto](https://gi
 
 ## Results
 
-|**Test**|**Result**|
+|**Benchmark**|**Result**|
 |--------|---------:|
 |BenchmarkGoLuaInit-4|1138 ns/op|
 |BenchmarkGoLuaModulo-4 |16658 ns/op|
 |BenchmarkGoLuaRecursion-4|52920 ns/op|
 |BenchmarkOttoInit-4|166096 ns/op|
-|BenchmarkOttoMath-4|1970924782 ns/op|
-|BenchmarkOttoStack-4|579791 ns/op|
+|BenchmarkOttoModulo-4|1970924782 ns/op|
+|BenchmarkOttoRecursion-4|579791 ns/op|
+|BenchmarkOttoCallOverhead-4|10624 ns/op|
+|BenchmarkOttoRealistic-4|15382 ns/op|
 
 Run the tests with `go test -bench=.`
--- otto_test.go
+++ otto_test.go
@@ -1,8 +1,18 @@
 package script
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/robertkrimen/otto"
+	"math/rand"
 	"testing"
 )
+
+type Frame struct {
+	Number   int
+	Name     string
+	Variable int
+}
 
 func BenchmarkOttoInit(b *testing.B) {
 	for i := 0; i < b.N; i++ {
@@ -10,15 +20,78 @@ func BenchmarkOttoInit(b *testing.B) {
 	}
 }
 
-func BenchmarkOttoMath(b *testing.B) {
+func BenchmarkOttoModulo(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		testOttoModulo()
 	}
 }
 
-func BenchmarkOttoStack(b *testing.B) {
+func BenchmarkOttoRecursion(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		testOttoRecursion()
+	}
+}
+
+func BenchmarkOttoCallOverhead(b *testing.B) {
+	vm := otto.New()
+	alerts := make(chan string, 0)
+	go func(chan string) {
+		for {
+			<-alerts
+		}
+	}(alerts)
+
+	vm.Set("alert", func(call otto.FunctionCall) otto.Value {
+		alerts <- call.Argument(0).String()
+		return otto.Value{}
+	})
+	vm.Run(`
+		var analyze = function(frame) {
+	`)
+
+	for i := 0; i < b.N; i++ {
+		frame := Frame{
+			Number: rand.Int(),
+		}
+		frame_bytes, _ := json.Marshal(frame)
+		vm.Run(fmt.Sprintf("analyze(%s)", string(frame_bytes)))
+	}
+}
+
+func BenchmarkOttoRealistic(b *testing.B) {
+	vm := otto.New()
+	alerts := make(chan string, 0)
+	go func(chan string) {
+		for {
+			<-alerts
+		}
+	}(alerts)
+
+	vm.Set("alert", func(call otto.FunctionCall) otto.Value {
+		alerts <- call.Argument(0).String()
+		return otto.Value{}
+	})
+	vm.Run(`
+		var max = 0
+		var map = {}
+
+		var analyze = function(frame) {
+			if (frame.Number > max)	{
+				alert("setting max")
+				max = frame.Number
+			}
+			map[frame.Name] = frame.Variable
+		}
+	`)
+
+	for i := 0; i < b.N; i++ {
+		frame := Frame{
+			Name:     "Foo",
+			Number:   rand.Int(),
+			Variable: rand.Int(),
+		}
+		frame_bytes, _ := json.Marshal(frame)
+		vm.Run(fmt.Sprintf("analyze(%s)", string(frame_bytes)))
 	}
 }
 
